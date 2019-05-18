53552ba03fbe18cfa01541938b85d8e2df7ddf7f
Kamil Polanski <polanski88@gmail.com>
2019-05-18 22:34:13 +0200

    Add task 15.5

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function Button(text) {
+    this.text = text;
+}
+Button.prototype = {
+    create: function() {
+        const self = this;
+        this.element = document.createElement(`button`);
+        this.element.innerText = this.text;
+        this.element.addEventListener(`click`, function() {
+            alert(self.text);
+        });
+        document.body.appendChild(this.element);
+    }
+}
+const btn1 = new Button(`Hello!`);
+btn1.create();
